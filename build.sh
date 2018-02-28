a72f18af41f11624f741327fe99b3a959f1b8d55
Aaron Klotz <aklotz@mozilla.com>
2018-02-27 17:58:29 -0700

    Firefox detection

---
+++ build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+cl -EHsc -Zi -DUNICODE -D_UNICODE -DWIN32_LEAN_AND_MEAN modload.cpp
--- modload.cpp
+++ modload.cpp
@@ -1,8 +1,13 @@
 #include <windows.h>
 #include <objbase.h>
+#include <pathcch.h>
 #include <shobjidl.h>
 #include <shellapi.h>
+#include <shlobj.h>
+#include <shlwapi.h>
 
+#include <algorithm>
+#include <iterator>
 #include <memory>
 #include <string>
 
@@ -11,13 +16,17 @@
 #include <comdef.h>
 #include <comip.h>
 
+#pragma comment(lib, "pathcch")
 #pragma comment(lib, "shell32")
+#pragma comment(lib, "shlwapi")
 
-struct ArgvDeleter
+#define TRY_DEFAULT_BROWSER
+
+struct LocalFreeDeleter
 {
-  void operator()(LPWSTR* aPtr)
+  void operator()(void* aPtr)
   {
-    ::LocalFree(reinterpret_cast<void*>(aPtr));
+    ::LocalFree(aPtr);
   }
 };
 
@@ -29,23 +38,41 @@ struct ComDeleter
   }
 };
 
-typedef std::unique_ptr<LPWSTR, ArgvDeleter> UniqueArgvPtr;
+typedef std::unique_ptr<wchar_t, LocalFreeDeleter> UniquePathPtr;
+typedef std::unique_ptr<LPWSTR, LocalFreeDeleter> UniqueArgvPtr;
 typedef std::unique_ptr<wchar_t, ComDeleter> UniqueComStringPtr;
-
-static const wchar_t* kCmdLineSwitches[] {
-  L"--modload-target-module",
-  L"--modload-target-firefox"
-};
 
 _COM_SMARTPTR_TYPEDEF(IApplicationAssociationRegistration,
                       IID_IApplicationAssociationRegistration);
+_COM_SMARTPTR_TYPEDEF(IFileOpenDialog, IID_IFileOpenDialog);
+_COM_SMARTPTR_TYPEDEF(IShellItem, IID_IShellItem);
+_COM_SMARTPTR_TYPEDEF(IShellItem2, IID_IShellItem2);
+
+static bool
+IsPathFirefox(const std::wstring& aPath)
+{
+  std::wstring testPath;
+
+  std::transform(aPath.begin(), aPath.end(), std::back_inserter(testPath),
+                 ::tolower);
+
+  std::wstring::size_type pos;
+  pos = testPath.find(L"firefox.exe",
+                      testPath.find_last_of(L'\\'));
+  return pos != std::wstring::npos;
+}
 
 static bool
 FindFirefox(std::wstring& aOutFirefoxPath)
 {
+  HRESULT hr;
+
+#if defined(TRY_DEFAULT_BROWSER)
+
+  // Try using the default http program
   IApplicationAssociationRegistrationPtr appRegInfo;
-  HRESULT hr = appRegInfo.CreateInstance(CLSID_ApplicationAssociationRegistration,
-                                         nullptr, CLSCTX_INPROC_SERVER);
+  hr = appRegInfo.CreateInstance(CLSID_ApplicationAssociationRegistration,
+                                 nullptr, CLSCTX_INPROC_SERVER);
   if (FAILED(hr)) {
     return false;
   }
@@ -58,7 +85,105 @@ FindFirefox(std::wstring& aOutFirefoxPath)
   }
 
   UniqueComStringPtr progId(rawProgId);
-  aOutFirefoxPath = rawProgId;
+
+  DWORD reqdNumChars = 0;
+  hr = ::AssocQueryString(ASSOCF_NOTRUNCATE | ASSOCF_INIT_IGNOREUNKNOWN,
+                          ASSOCSTR_EXECUTABLE, rawProgId, L"open", nullptr,
+                          &reqdNumChars);
+  if (FAILED(hr) || hr != S_FALSE) {
+    return false;
+  }
+
+  auto exeBuf = std::make_unique<wchar_t[]>(reqdNumChars);
+  if (!exeBuf) {
+    return false;
+  }
+
+  hr = ::AssocQueryString(ASSOCF_NOTRUNCATE | ASSOCF_INIT_IGNOREUNKNOWN,
+                          ASSOCSTR_EXECUTABLE, rawProgId, L"open", exeBuf.get(),
+                          &reqdNumChars);
+  if (FAILED(hr)) {
+    return false;
+  }
+
+  if (IsPathFirefox(exeBuf.get())) {
+    aOutFirefoxPath = exeBuf.get();
+    return true;
+  }
+
+#endif // defined(TRY_DEFAULT_BROWSER)
+
+  // Uh-oh. We can't find Firefox! Show a picker?
+  IFileOpenDialogPtr picker;
+  hr = picker.CreateInstance(CLSID_FileOpenDialog, nullptr, CLSCTX_INPROC_SERVER);
+  if (FAILED(hr)) {
+    return true;
+  }
+
+  IShellItem2Ptr programDir;
+  hr = ::SHGetKnownFolderItem(FOLDERID_ProgramFiles, KF_FLAG_DEFAULT,
+                              nullptr, IID_IShellItem2, (void**) &programDir);
+  if (FAILED(hr)) {
+    return false;
+  }
+
+  hr = picker->SetFolder(programDir);
+  if (FAILED(hr)) {
+    return false;
+  }
+
+  COMDLG_FILTERSPEC fltSpec = {L"Executable Files", L"*.exe"};
+  hr = picker->SetFileTypes(1, &fltSpec);
+  if (FAILED(hr)) {
+    return hr;
+  }
+
+  hr = picker->SetFileTypeIndex(1); // one-based
+  if (FAILED(hr)) {
+    return hr;
+  }
+
+  hr = picker->SetFileName(L"firefox.exe");
+  if (FAILED(hr)) {
+    return hr;
+  }
+
+  hr = picker->SetOptions(FOS_STRICTFILETYPES | FOS_FORCEFILESYSTEM |
+                          FOS_FILEMUSTEXIST | /*FOS_SHAREAWARE |*/
+                          FOS_DONTADDTORECENT);
+  if (FAILED(hr)) {
+    return false;
+  }
+
+  hr = picker->SetTitle(L"Location of Firefox Executable");
+  if (FAILED(hr)) {
+    return false;
+  }
+
+  // Modal
+  hr = picker->Show(nullptr);
+  if (FAILED(hr)) {
+    return false;
+  }
+
+  IShellItemPtr selection;
+  hr = picker->GetResult(&selection);
+  if (FAILED(hr)) {
+    return false;
+  }
+
+  LPWSTR rawFileName;
+  hr = selection->GetDisplayName(SIGDN_FILESYSPATH, &rawFileName);
+  if (FAILED(hr)) {
+    return false;
+  }
+
+  UniqueComStringPtr fileName(rawFileName);
+  if (!IsPathFirefox(rawFileName)) {
+    return false;
+  }
+
+  aOutFirefoxPath = rawFileName;
   return true;
 }
 
@@ -67,6 +192,11 @@ GetTargetModule(std::wstring& aOutTargetModule)
 {
   return true;
 }
+
+static const wchar_t* kCmdLineSwitches[] {
+  L"--modload-target-module",
+  L"--modload-target-firefox"
+};
 
 static bool
 GetBinaries(int aArgc, LPWSTR* aArgv, std::wstring& aOutFirefoxPath,
@@ -83,7 +213,15 @@ GetBinaries(int aArgc, LPWSTR* aArgv, std::wstring& aOutFirefoxPath,
     }
 
     if (!wcscmp(aArgv[i], kCmdLineSwitches[1]) && (i + 1) < aArgc) {
-      aOutFirefoxPath = aArgv[i + 1];
+      PWSTR rawCanonical;
+      HRESULT hr = ::PathAllocCanonicalize(aArgv[i + 1], PATHCCH_NONE,
+                                           &rawCanonical);
+      if (FAILED(hr)) {
+        return false;
+      }
+
+      UniquePathPtr canonical(rawCanonical);
+      aOutFirefoxPath = rawCanonical;
       ++i;
       continue;
     }
@@ -149,6 +287,10 @@ wWinMain(HINSTANCE aInstance, HINSTANCE aPrevInstance, PWSTR aCmdLine,
   if (!GetBinaries(argc, argv.get(), firefoxBinary, targetModule)) {
     return 2;
   }
+
+  std::wstring output(firefoxBinary);
+  output += L'\n';
+  ::OutputDebugStringW(output.c_str());
 
   return 0;
 }
